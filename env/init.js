533cbe11c3d676d59072fbc353834863c0eaa0c8
Kishan Devani <kdevani964@gmail.com>
2018-11-21 22:37:46 +0530

    Update modules & added gitignore.

--- env/init.js
+++ env/init.js
@@ -1,5 +1,5 @@
 window.login = require('../src/main/login.js');
 window.home = require('../src/home/home.js');
 window.test = require('../src/test/test.jsx');
-window.settings = require('../src/settings/settings');
+window.settings = require('../src/settings/settings.js');
 window.reactApp = require('../src/ReactEX/index.jsx');
